3e266259e6883551c0c7b3ef7d5a351f7287b4cd
(Handehdy) <[handehudayioglu@gmail.com]>
2022-07-05 00:32:51 +0300

    first commit

---
+++ src/com/bilgeadam/boost/patika/Taximeter.java
@@ -0,0 +1,25 @@
+package com.bilgeadam.boost.patika;
+
+import java.util.Scanner;
+
+public class Taximeter {
+
+	public static void main(String[] args) {
+		
+		double startPrice=10;
+		double perKm=2.20;
+		double total=10;
+		
+		System.out.println("Enter the distance you will travel: ");
+		Scanner input = new Scanner (System.in);
+		int distance = input.nextInt();
+		
+		total = distance*perKm;
+		total+=startPrice;
+		
+				
+		total = (total<20) ? 20 : total;
+		System.out.println("Amount you have to pay: " +total+ " TL");
+
+}
+}
