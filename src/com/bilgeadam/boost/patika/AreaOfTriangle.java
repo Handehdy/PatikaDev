fd42107fe35a7c1aaa93497a34ab80017ba1ae5e
(Handehdy) <[handehudayioglu@gmail.com]>
2022-07-05 00:26:41 +0300

    first commit

---
+++ src/com/bilgeadam/boost/patika/AreaOfTriangle.java
@@ -0,0 +1,40 @@
+package com.bilgeadam.boost.patika;
+
+import java.util.Scanner;
+import java.lang.*;
+
+public class AreaOfTriangle {
+
+	public static void main(String[] args) {
+		
+		int edge1,edge2,edge3;
+		double u;
+		double area;
+		double result;
+		
+		
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the lengths of the right edges of the triangle. ");
+		System.out.print("First edge: ");
+		edge1=input.nextInt();
+		System.out.print("Second edge: ");
+		edge2=input.nextInt();
+		System.out.print("Third edge: ");
+		edge3=input.nextInt();
+		
+		
+		u = (edge1+edge2+edge3)/2;
+		result =( u*(u-edge1)*(u-edge2)*(u-edge3));
+		area = Math.sqrt(result);
+		System.out.println("Area of your triangle: "+area);
+		
+		
+		
+		
+		
+	    
+		
+	}
+
+}
