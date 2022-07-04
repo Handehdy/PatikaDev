fe594d75d84316aa832b09a89a31ccb8d92f1dfa
(Handehdy) <[handehudayioglu@gmail.com]>
2022-07-05 00:11:44 +0300

    first commit

---
+++ src/com/bilgeadam/boost/patika/VAT.java
@@ -0,0 +1,36 @@
+package com.bilgeadam.boost.patika;
+
+import java.util.Scanner;
+
+public class VAT {
+
+	public static void main(String[] args) {
+		
+	double amount;
+	double amountWithVat;
+	double vat;
+	
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter the amount: ");
+	amount = input.nextDouble();
+	
+	
+	if((0<amount) && (amount<1000)) {
+		vat = (amount*18)/100;
+		amountWithVat=amount+vat;
+		System.out.println("VAT amound: " + vat);
+		System.out.println("Amound with VAT: " + amountWithVat);
+	}
+	else {
+		
+		vat = (amount*8) /100;
+		amountWithVat=amount+vat;
+		System.out.println("VAT amound: " + vat);
+		System.out.println("Amound with VAT: " + amountWithVat);
+		
+		
+	}
+	
+	}
+
+}
