f531945377c42a5e9ff10aae34597677acf87084
(Handehdy) <[handehudayioglu@gmail.com]>
2022-07-05 00:56:03 +0300

    first commit

---
+++ src/com/bilgeadam/boost/patika/BMI.java
@@ -0,0 +1,29 @@
+package com.bilgeadam.boost.patika;
+
+import java.util.Scanner;
+
+public class BMI {
+
+	public static void main(String[] args) {
+
+		double height, weight, BMI;
+		
+
+		Scanner scanner = new Scanner(System.in);
+
+		
+
+		System.out.print("Enter your height(meter) :");
+		height = scanner.nextDouble();
+
+		System.out.print("Enter your weight :");
+		weight = scanner.nextDouble();
+
+		BMI = weight / (height * height);
+		
+		System.out.println("Your body mass index: "+BMI);
+
+		
+		}
+	}
+
